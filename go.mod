ba8ae5154b5e45efa54d4c67e9ce3e91157521c2
OlegTatarenko <2206941@gmail.com>
2023-03-24 15:22:51 +0300

    c = a + b

---
+++ go.mod
@@ -0,0 +1,3 @@
+module Git_test_2
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a, b, c int
+	c = a + b //дополнил текущий комит этим комментарием
+	fmt.Println(c)
+}
